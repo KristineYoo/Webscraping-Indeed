24fbb5c51f914eb796ba034a05e9a8e06bb3e740
Kristine <kly35@drexel.edu>
2025-05-11 19:03:58 -0400

    parse content

--- webscrape.py
+++ webscrape.py
@@ -5,3 +5,7 @@ import pandas as pd
 
 #http Request to website
 url = 'https://www.indeed.com/jobs?q=softare+engineer&l=New+York+city%2C+NY&from=searchOnHP&vjk=1d105def0857ac5e'
+response = requests.get(url)
+
+#parse content using beautiful soup 
+soup = BeautifulSoup(response.text, 'html.parser')
