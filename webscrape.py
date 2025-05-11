8f7e5bd41c6f0c2ca528939d016eaad912775384
Kristine <kly35@drexel.edu>
2025-05-11 19:05:27 -0400

    extracting the data

--- webscrape.py
+++ webscrape.py
@@ -9,3 +9,15 @@ response = requests.get(url)
 
 #parse content using beautiful soup 
 soup = BeautifulSoup(response.text, 'html.parser')
+
+#extracing the data 
+job_titles = soup.find_all('a', class_='jobtitle')
+companies = soup.find_all('span', class_='company')
+locations = soup.find_all('div', class_='location')
+summaries = soup.find_all('div', class_='summary')
+
+# Print the first job title, company, location, and summary
+print(job_titles[0].text)
+print(companies[0].text)
+print(locations[0].text)
+print(summaries[0].text)
